e09a821537d7851762c50b5f3ae0859e27f29f96
Long Nguyễn <huylong.tk123@gmail.com>
2024-10-31 15:09:32 +0700

    Add Main Window

---
+++ FLauncher/CC/tags.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FLauncher.CC
+{
+    /// <summary>
+    /// Interaction logic for tags.xaml
+    /// </summary>
+    public partial class tags : UserControl
+    {
+        public tags()
+        {
+            InitializeComponent();
+        }
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(tags));
+
+
+    }
+}
