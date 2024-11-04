bfc10bbf98520d4082d5d918a8207ba439d579f1
Thien Le <thienle2105@gmail.com>
2024-11-04 15:54:32 +0700

    ok

--- FLauncher/Views/Login.xaml.cs
+++ FLauncher/Views/Login.xaml.cs
@@ -19,9 +19,10 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 using static System.Net.WebRequestMethods;
 using FLauncher.Services;
+using Microsoft.VisualBasic.ApplicationServices;
 
 
-namespace FLauncher
+namespace FLauncher.Views
 {
     public partial class Login : Window
     {
@@ -37,6 +38,10 @@ namespace FLauncher
         {
             PerformLogin();
         }
+        private void LoginGG_Click(object sender, RoutedEventArgs e)
+        {
+            //add sau
+        }
 
         private async Task<string> CheckLogin(string emailUser, string password)
         {
@@ -51,8 +56,9 @@ namespace FLauncher
                     {
                         return "admin";
                     }
-                    else if (user.Role == 3)
+                    else if (user.Role == 3 || user.Role == 2)
                     {
+                        //SaveUserInfoToJson(user.Email);
                         return "customer";
                     }
                 }
@@ -64,6 +70,22 @@ namespace FLauncher
             {
                 MessageBox.Show("Lỗi khi kết nối với cơ sở dữ liệu: " + ex.Message);
                 return null;
+            }
+        }
+
+        private void SaveUserInfoToJson()
+        {
+            try
+            {
+                // Convert user object to JSON format
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(user, Formatting.Indented);
+
+                // Save JSON to file
+                System.IO.File.WriteAllText("userInfo.json", json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi lưu thông tin người dùng: " + ex.Message);
             }
         }
 
@@ -96,24 +118,32 @@ namespace FLauncher
                 MessageBox.Show("Đăng nhập thành công với tư cách quản trị viên!");
                 MainWindow adminWindow = new MainWindow();
                 adminWindow.Show();
+
+                // Close the Login window
+                this.Close();
             }
             else if (accountType == "customer")
             {
                 MessageBox.Show("Đăng nhập thành công với tư cách khách hàng!");
-                CustomerWindow customerWindow = new CustomerWindow();
+                Model.User loggedInUser = await _userService.GetUserByEmailPass(enteredUserEmail, enteredPassword);
+                CustomerWindow customerWindow = new CustomerWindow(loggedInUser.Id);
                 customerWindow.Show();
+
+                this.Close();
             }
             else
             {
                 MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng.");
+                /*
+                 // Close the Login window
+                Window parentWindow = Window.GetWindow(this);
+                if (parentWindow != null)
+                {
+                    parentWindow.Close();
+                }
+                 */
             }
 
-            // Close the Login window
-            Window parentWindow = Window.GetWindow(this);
-            if (parentWindow != null)
-            {
-                parentWindow.Close();
-            }
         }
     }
 
--- FLauncher/Views/MainWindow.xaml.cs
+++ FLauncher/Views/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,16 +6,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FLauncher
+namespace FLauncher.Views
 {
     /// <summary>
-    /// Interaction logic for CustomerWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class CustomerWindow : Window
+    public partial class MainWindow : Window
     {
-        public CustomerWindow()
+        public MainWindow()
         {
             InitializeComponent();
         }
