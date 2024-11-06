52d2aaecb0036cdb540179e3acb858c57bb3533e
viet81918 <viet81918@gmail.com>
2024-11-06 13:28:22 +0700

    ok

---
+++ FLauncher/DAO/FriendDAO.cs
@@ -0,0 +1,31 @@
+﻿using FLauncher.Model;
+using FLauncher.Services;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLauncher.DAO
+{
+    public class FriendDAO : SingletonBase<FriendDAO>
+    {
+        private readonly FlauncherDbContext _dbContext;
+
+        public FriendDAO()
+        {
+            var connectionString = "mongodb://localhost:27017/";
+            var client = new MongoClient(connectionString);
+            _dbContext = FlauncherDbContext.Create(client.GetDatabase("FPT"));
+        }
+        public List<Friend> GetFriendInvitationsforGamer(Gamer gamer)
+        {
+           
+            return _dbContext.Friends
+                .Where(friend => friend.AcceptId == gamer.GamerId)
+                .ToList();
+        }
+
+    }
+}
---
+++ FLauncher/DAO/GameDAO.cs
@@ -0,0 +1,27 @@
+﻿using FLauncher.Model;
+using FLauncher.Services;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLauncher.DAO
+{
+    public class GameDAO : SingletonBase<GameDAO>
+    {
+        private readonly FlauncherDbContext _dbContext;
+
+        public GameDAO()
+        {
+            var connectionString = "mongodb://localhost:27017/";
+            var client = new MongoClient(connectionString);
+            _dbContext = FlauncherDbContext.Create(client.GetDatabase("FPT"));
+        }
+        public List<Game> GetGames()
+        {
+            return _dbContext.Games.ToList();
+        }
+    }
+}
